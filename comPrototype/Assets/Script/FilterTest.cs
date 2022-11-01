a2712ab499a25abd3403fc199ae02fb654b6cbd7
ansdudtjd <ansdudtjd123@naver.com>
2022-11-01 12:59:20 +0900

    진행상황 사라짐
    진행상황 사라짐

---
+++ comPrototype/Assets/Script/FilterTest.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FilterTest : MonoBehaviour
+{
+    public SpriteRenderer spriteRender;
+
+    void Start()
+    {
+        spriteRender.color = new Color(0,1,1, 0.4f);
+    }
+
+    void Update()
+    {
+        
+    }
+}
