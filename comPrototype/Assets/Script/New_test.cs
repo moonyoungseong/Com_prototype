b7e32d6b304ef7599591cfb0eb131c74aad301e4
moonyoungseong <ansdudtjd123@naver.com>
2022-11-07 01:16:21 +0900

    새로운 구상 씬으로 추가
    UI만 해놓음

---
+++ comPrototype/Assets/Script/New_test.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class New_test : MonoBehaviour
+{
+    public List<Text> buttonvalue = new List<Text>();
+    public int recent; // 최신값
+    public int answer;
+    public int user;
+    public int total;
+
+    public void One()
+    {
+        buttonvalue[0] = 
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
